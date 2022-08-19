7dd17eb9d619aa3d15a4956f62aa24e7dbaea066
noellerjd <noellerninja@gmail.com>
2022-08-18 22:10:14 -0600

    view and add department functioning

--- db/schema.sql
+++ db/schema.sql
@@ -1,1 +1,16 @@
-DROP DATABASE IF EXISTS department_db
+DROP DATABASE IF EXISTS department_db;
+CREATE DATABASE department_db;
+
+USE department_db;
+
+CREATE TABLE departments (
+    id INT NOT NULL AUTO_INCREMENT,
+
+    name VARCHAR (30) NOT NULL,
+
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE SALES AS 
+SELECT id, name
+FROM departments
--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 // Import and require mysql2
 const express = require("express");
-// const mysql = require("mysql2");
+const mysql = require("mysql2");
+require("dotenv").config();
 // const dotenv = require("dotenv");
 const inquirer = require("inquirer");
-const { Sequelize, DataTypes } = require("sequelize");
-const sequelize = new Sequelize("sqlite::memory:");
+// const { Sequelize, DataTypes } = require("sequelize");
+// const sequelize = new Sequelize("sqlite::memory:");
 // const fs = require("fs");
 
 const PORT = process.env.PORT || 3001;
@@ -13,33 +14,124 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-console.log(User === sequelize.models.User);
+var con = mysql.createConnection({
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+});
 
+// Initial Prompt
 const init = () => {
   inquirer
     .prompt([
       {
         type: "list",
         name: "options",
-        message: "Add or View Departments?",
+        message: "Select an option",
         choices: [
           {
             name: "View Departments",
             value: "viewDepartment",
           },
           {
+            name: "View Employees",
+            value: "viewEmployees",
+          },
+          {
             name: "Add Department",
             value: "addDepartment",
+          },
+          {
+            name: "Add Employee",
+            value: "addEmployee",
           },
         ],
       },
     ])
+    // View Departments
     .then((response) => {
       if (response.options === "viewDepartment") {
-        console.log(`View Department Code ${response.options}`);
+        con.connect(function (err) {
+          if (err) throw err;
+          console.log("Connected!");
+          con.query(
+            "SELECT * FROM departments",
+            function (err, result, fields) {
+              if (err) throw err;
+              let departmentList = [];
+              result.forEach((element) => {
+                departmentList.push({
+                  name: element.name,
+                  value: element.name,
+                });
+              });
+              inquirer
+                .prompt([
+                  {
+                    type: "list",
+                    name: "departmentName",
+                    message: "Which department would you like to select?",
+                    choices: departmentList,
+                  },
+                ])
+                .then((response) => {
+                  let department = response.departmentName;
+                  inquirer
+                    .prompt([
+                      {
+                        type: "list",
+                        name: "selectAction",
+                        message:
+                          "What would you like to do with this department?",
+                        choices: [
+                          {
+                            name: "View Employees",
+                            value: "viewEmployees",
+                          },
+                          {
+                            name: "Add Employee",
+                            value: "addEmployee",
+                          },
+                          {
+                            name: `Delete ${response.departmentName} department`,
+                            value: "deleteDepartment",
+                          },
+                        ],
+                      },
+                    ])
+                    .then((response) => {
+                      switch (response.selectAction) {
+                        case "viewEmployees":
+                          break;
+
+                        case "addEmployee":
+                          break;
+
+                        case "deleteDepartment":
+                          var sql = `DELETE FROM departments WHERE name = "${department}" `;
+                          con.query(sql, function (err, result) {
+                            if (err) throw err;
+                            console.log(
+                              `${department} deleted from department database`
+                            );
+                            console.log("-------------------------");
+                            console.log("Returning to main menu...");
+                            console.log("-------------------------");
+                            init();
+                          });
+                          break;
+
+                        default:
+                          break;
+                      }
+                    });
+                });
+            }
+          );
+        });
       }
+      // Add Departments
       if (response.options === "addDepartment") {
-        // console.log(`Add Department Code ${response.options}`);
         inquirer
           .prompt([
             {
@@ -65,7 +157,25 @@ const init = () => {
           ])
           .then((response) => {
             if (response.confirm === "confirm") {
-              console.log(`Add Department ${response.confirm}`);
+              //   console.log(`Add Department ${response.departmentName}`);
+              con.connect(function (err) {
+                if (err) throw err;
+                console.log("Connected!");
+                var sql = `INSERT INTO departments (name) VALUES ('${response.departmentName}')`;
+                con.query(sql, function (err, result) {
+                  if (err) throw err;
+                  console.log("-------------------------");
+                  console.log(
+                    `Added ${response.departmentName} to the database`
+                  );
+                  console.log("-------------------------");
+                  //   console.log(result);
+                  console.log("-------------------------");
+                  console.log("Returning to main menu...");
+                  console.log("-------------------------");
+                  init();
+                });
+              });
             }
             if (response.confirm === "deny") {
               console.log("-------------------------");
@@ -80,4 +190,4 @@ const init = () => {
 
 init();
 
-console.log(process.env);
+// console.log(process.env);
